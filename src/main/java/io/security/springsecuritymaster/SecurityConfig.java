f4cfb1db4c34c72bbccd2ea700a1b36dbc6bdc61
YKJ <rudwns7552@gmail.com>
2024-04-05 17:40:27 +0900

    feat: 인증 프로세스

--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -16,6 +16,23 @@ public class SecurityConfig {
     http
         .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
         .formLogin(Customizer.withDefaults());
+    
+//        .formLogin(form -> form
+////            .loginPage("/loginPage")
+//            .loginProcessingUrl("/loginProc")
+//            .defaultSuccessUrl("/", true)
+//            .failureUrl("/failed")
+//            .usernameParameter("userId")
+//            .passwordParameter("passwd")
+//            .successHandler((request, response, authentication) -> {  //defaultSuccessUrl 보다 우선
+//              System.out.println("authentication : " + authentication);
+//              response.sendRedirect("/home");
+//            })
+//            .failureHandler((request, response, exception) -> {       //failureUrl 보다 우선
+//              System.out.println("exception = " + exception.getMessage());
+//              response.sendRedirect("/login");
+//            })
+//            .permitAll());
 
     return http.build();
   }
