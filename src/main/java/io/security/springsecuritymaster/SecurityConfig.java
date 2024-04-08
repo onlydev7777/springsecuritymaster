715a1146e8228a7b809707836e1251ae77d71d04
onlydev7777 <onlydev7777@gmail.com>
2024-04-08 16:21:43 +0900

    feat: 인증 프로세스 md파일 작성

--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -14,25 +14,12 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-        .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-        .formLogin(Customizer.withDefaults());
-    
-//        .formLogin(form -> form
-////            .loginPage("/loginPage")
-//            .loginProcessingUrl("/loginProc")
-//            .defaultSuccessUrl("/", true)
-//            .failureUrl("/failed")
-//            .usernameParameter("userId")
-//            .passwordParameter("passwd")
-//            .successHandler((request, response, authentication) -> {  //defaultSuccessUrl 보다 우선
-//              System.out.println("authentication : " + authentication);
-//              response.sendRedirect("/home");
-//            })
-//            .failureHandler((request, response, exception) -> {       //failureUrl 보다 우선
-//              System.out.println("exception = " + exception.getMessage());
-//              response.sendRedirect("/login");
-//            })
-//            .permitAll());
+        .authorizeHttpRequests(auth -> auth
+            .anyRequest().authenticated()
+        )
+        .formLogin(Customizer.withDefaults())
+
+    ;
 
     return http.build();
   }
