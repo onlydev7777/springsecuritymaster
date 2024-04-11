775c2de202ff81bd12c560060eff01862d69f3f7
onlydev7777 <onlydev7777@gmail.com>
2024-04-11 11:08:50 +0900

    feat: - section3   1) 인증 아키텍쳐.md 파일 작성      - AuthenticationManager 이해

--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -1,26 +1,47 @@
-package io.security.springsecuritymaster;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@EnableWebSecurity
-@Configuration
-public class SecurityConfig {
-
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .authorizeHttpRequests(auth -> auth
-            .anyRequest().authenticated()
-        )
-        .formLogin(Customizer.withDefaults())
-
-    ;
-
-    return http.build();
-  }
-}
+//package io.security.springsecuritymaster;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@EnableWebSecurity
+//@Configuration
+//public class SecurityConfig {
+//
+//  @Bean
+//  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+////    http
+////        .authorizeHttpRequests(auth -> auth
+////            .anyRequest().authenticated()
+////        )
+////        .formLogin(Customizer.withDefaults())
+////    ;
+//
+//    AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(
+//        AuthenticationManagerBuilder.class);
+//
+//    AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+//
+//    http
+//        .authorizeHttpRequests(auth -> auth
+//            .requestMatchers("/", "/api/login").permitAll()
+//            .anyRequest().authenticated())
+//        .authenticationManager(authenticationManager)
+//        .addFilterBefore(customFilter(http, authenticationManager),
+//            UsernamePasswordAuthenticationFilter.class);
+//
+//    return http.build();
+//  }
+//
+//  private CustomAuthenticationFilter customFilter(HttpSecurity http,
+//      AuthenticationManager authenticationManager) {
+//    CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(http);
+//    customAuthenticationFilter.setAuthenticationManager(authenticationManager);
+//    return customAuthenticationFilter;
+//  }
+//}
---
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig2.java
@@ -0,0 +1,43 @@
+package io.security.springsecuritymaster;
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AnonymousAuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig2 {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/", "/api/login").permitAll()
+            .anyRequest().authenticated())
+        .addFilterBefore(customFilter(http), UsernamePasswordAuthenticationFilter.class);
+    return http.build();
+  }
+
+  public CustomAuthenticationFilter customFilter(HttpSecurity http) {
+
+    List<AuthenticationProvider> list1 = List.of(new DaoAuthenticationProvider());
+    ProviderManager parent = new ProviderManager(list1);
+    List<AuthenticationProvider> list2 = List.of(new AnonymousAuthenticationProvider("key"),
+        new CustomAuthenticationProvider());
+    ProviderManager authenticationManager = new ProviderManager(list2, parent);
+
+    CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(http);
+    customAuthenticationFilter.setAuthenticationManager(authenticationManager);
+
+    return customAuthenticationFilter;
+
+  }
+}
