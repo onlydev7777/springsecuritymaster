47304843960774fca1cc1cc2f29332c1149e6516
onlydev7777 <onlydev7777@gmail.com>
2024-04-16 17:55:44 +0900

    feat: - section5 세션관리   1) 세션관리.md 파일 작성      - 동시세션 제어      - 세션 고정보호 전략      - 세션 생성 정책      - SessionManagementFilter      - ConcurrentSessionFilter

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,5 +1,7 @@
 package io.security.springsecuritymaster;
 
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
@@ -8,21 +10,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class IndexController {
 
+  private final SessionInfoService sessionInfoService;
+
   @GetMapping("/")
-  public String index() {
-    return "index";
+  public Authentication index(Authentication authentication, HttpSession session) {
+    return authentication;
   }
+
+  @GetMapping("/sessionInfo")
+  public void sessionInfo() {
+    sessionInfoService.sessionInfo();
+  }
+
 
   @GetMapping("/loginPage")
-  public String loginPage() {
+  public String login() {
     return "loginPage";
-  }
-
-  @GetMapping("/home")
-  public String home() {
-    return "home";
   }
 
   @GetMapping("/anonymous")
@@ -30,16 +36,33 @@ public class IndexController {
     return "anonymous";
   }
 
+  @GetMapping("/authentication")
+  public String authentication(Authentication authentication) {
+
+    if (authentication instanceof AnonymousAuthenticationToken) {
+      return "anonymous";
+    } else {
+      return "null";
+    }
+  }
+
   @GetMapping("/anonymousContext")
   public String anonymousContext(@CurrentSecurityContext SecurityContext context) {
     return context.getAuthentication().getName();
   }
 
-  @GetMapping("/authentication")
-  public String authentication(Authentication authentication) {
-    if (authentication instanceof AnonymousAuthenticationToken) {
-      return "anonymous";
-    }
-    return "not anonymous";
+  @GetMapping("/logoutSuccess")
+  public String logoutSuccess(@CurrentSecurityContext SecurityContext context) {
+    return "logoutSuccess";
+  }
+
+  @GetMapping("/invalidSessionUrl")
+  public String invalidSessionUrl() {
+    return "invalidSessionUrl";
+  }
+
+  @GetMapping("/expired")
+  public String expired() {
+    return "expired";
   }
 }
