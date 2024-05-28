5064f610c0ef8624974b2efe1bd2f9abed87ab65
onlydev7777 <onlydev7777@gmail.com>
2024-05-28 10:22:46 +0900

    feat: 커스텀 인증 success handler
    1) FormAuthenticationSuccessHandler
       - SimpleUrlAuthenticationSuccessHandler 상속 클래스 작성
       - 인증 성공 시 이전 요청 값으로 redirect 처리

--- src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
@@ -1,6 +1,7 @@
 package io.security.springsecuritymaster.security.configs;
 
 import io.security.springsecuritymaster.security.details.FormAuthenticationDetailsSource;
+import io.security.springsecuritymaster.security.handler.FormAuthenticationSuccessHandler;
 import io.security.springsecuritymaster.security.provider.FormAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,7 @@ public class SecurityConfig {
 
   private final FormAuthenticationProvider formAuthenticationProvider;
   private final FormAuthenticationDetailsSource formAuthenticationDetailsSource;
+  private final FormAuthenticationSuccessHandler formAuthenticationSuccessHandler;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -28,6 +30,7 @@ public class SecurityConfig {
         .formLogin(form -> form
             .loginPage("/login").permitAll()
             .authenticationDetailsSource(formAuthenticationDetailsSource)
+            .successHandler(formAuthenticationSuccessHandler)
         )
         .authenticationProvider(formAuthenticationProvider)
     ;
