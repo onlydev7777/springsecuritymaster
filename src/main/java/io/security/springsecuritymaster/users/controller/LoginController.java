67ee5c00d32d83ecced323ff8a85380ee6eba97c
onlydev7777 <onlydev7777@gmail.com>
2024-05-28 09:46:36 +0900

    feat: 1) 로그아웃 기능 구현

--- src/main/java/io/security/springsecuritymaster/users/controller/LoginController.java
+++ src/main/java/io/security/springsecuritymaster/users/controller/LoginController.java
@@ -1,5 +1,10 @@
 package io.security.springsecuritymaster.users.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -14,5 +19,15 @@ public class LoginController {
   @GetMapping(value = "/signup")
   public String signup() {
     return "login/signup";
+  }
+
+  @GetMapping(value = "/logout")
+  public String logout(HttpServletRequest request, HttpServletResponse response) {
+    Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+    if (authentication != null) {
+      SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+      securityContextLogoutHandler.logout(request, response, authentication);
+    }
+    return "redirect:/login";
   }
 }
